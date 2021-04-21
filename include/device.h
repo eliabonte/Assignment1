e3d88f2c7681599089be0f076264bb3f8cc5d450
elia <elia.bontempelli@studenti.unitn.it>
2021-04-21 09:37:24 +0200

    eb_init and eb_checkConstraints done #3

--- include/device.h
+++ include/device.h
@@ -1,18 +1,27 @@
 #ifndef DEVICE_H
 #define DEVICE_H
 
-/*
- * A C-style library for fractions
- */
-
 /**
  * 
  * A struct representing the device  
 **/
-struct device {
-    
+struct Device {
+    double length_shaft;
+    double width_towtruck;
+    double width_platform;
+    double rotation;
+    double sliding;
 };
 
+/**
+    A function initing a device 
+**/
+Device* eb_init(double length_shaft, double width_towtruck, double width_platform, double rotation, double sliding);
+
+/**
+    A function checking costraints  
+**/
+bool eb_checkCostraints(double length_shaft, double width_towtruck, double width_platform, double rotation, double sliding);
 
 
 
--- src/MyDevice.cpp
+++ src/MyDevice.cpp
@@ -3,9 +3,64 @@
 #include "iostream"
 
 using namespace std;
-/*
- *  Source for the not-part-of-a-library class Fraction
- *
- */
+
+
+/**
+    A function initing a device 
+**/
+Device* eb_init(double length_shaft, double width_towtruck, double width_platform, double rotation, double sliding){
+    /*
+        some components' features are fixed --> shaft width, towtruck height, platform height 
+    */
+   double std_shaftWidth = 40;
+   double std_towtruckHeight = 50;
+   double std_platformHeight = 20;
+    
+    Device* eb_device = new Device;
+
+    if(eb_checkCostraints(length_shaft,width_towtruck,width_platform,rotation,sliding)==false){
+        eb_device==NULL;
+    }
+    else{
+        eb_device -> length_shaft;
+        eb_device -> width_towtruck;
+        eb_device ->  width_platform;
+        eb_device ->  rotation;
+        eb_device ->  sliding;
+    }
+
+    return eb_device;
+}
+
+/**
+    A function checking costraints  
+**/
+bool eb_checkCostraints(double length_shaft, double width_towtruck, double width_platform, double rotation, double sliding){
+    
+    /*
+        some components' features are fixed --> shaft width, towtruck height, platform height 
+    */
+   double std_shaftWidth = 40;
+   double std_towtruckHeight = 50;
+   double std_platformHeight = 20;
+    
+    if(length_shaft < 0 || width_towtruck < 0 || width_platform < 0){
+        return false;
+    }
+
+    if(rotation > 80 || rotation < -80){
+        return false;
+    }
+
+    if(width_towtruck < std_shaftWidth){
+        return false;
+    }
+
+    if(width_platform < std_shaftWidth){
+        return false;
+    }
+
+    return true;
+}
 
 
