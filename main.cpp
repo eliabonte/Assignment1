c208ac83bd91581d5036acce93efb96e6bc66075
elia <elia.bontempelli@studenti.unitn.it>
2021-04-22 09:27:44 +0200

    bug svg fixed + alcune prove nel main (funziona)

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ int main() {
 
     Device* myDevice = new Device;
 
-    myDevice=eb_init(400,160,300,60,480);
+    myDevice=eb_init(400,160,300,-60,480);
 
     cout<<"creo un file svg del mio device con parametri: "<<endl;
     cout<<myDevice->length_shaft<<" , "<<myDevice->width_towtruck<<" , "<<myDevice->width_platform<<" , "<<myDevice->rotation<<" , "<<myDevice->sliding<<endl;
@@ -23,8 +23,15 @@ int main() {
     cout<<"Xplatform: "<<eb_Xplatform(myDevice)<<endl;
     cout<<"Yplatform: "<<eb_Yplatform(myDevice)<<endl;
     
-    
-    eb_save_to_file(myDevice);
+    eb_save_to_file(myDevice,"bella");
+
+    cout<<"cambio adesso la rotzione e poi creo nuovo file: -60 -> 30"<<endl;
+
+    eb_set_rotation(myDevice,30);
+    eb_save_to_file(myDevice,"bellax2");
+
+
+
 
     return 0;
 }
